3c61fca1d981be6d77b3f25827ce3ddf4dbaa800
Mendozawendy <60940669+Mendozawendy@users.noreply.github.com>
2021-02-12 23:38:20 -0600

    Add files via upload

--- testgenSFMSig.m
+++ testgenSFMSig.m
@@ -20,11 +20,12 @@ nSamples = length(timeVec);
 %Signal sample at 5*maximum frequency
 genPlotSig(5,argsvector)
 
+
 %Signal smapled at 4*maximum frequency
 genPlotSig(4,argsVector)
 
 % Generate the Step FM signal
-sigVec = gensfmsig(timeVec{5},A,[timea{5},freq0{4},freq1{4}]);
+sigVec = gensfmsig(timeVec,A,timea,freq0,freq1);
 
 %Plot the signal
 subplot(1,5,4);
@@ -34,7 +35,46 @@ title('Step FM Signal')
 xlabel('Magnitude')
 ylabel('Time Sample')
 
+%Plot the periodogram
+%--------------
+%Length of data 
+dataLen = timeVec(end)-timeVec(1);
+%DFT sample corresponding to Nyquist frequency
+kNyq = floor(nSamples/2)+1;
+% Positive Fourier frequencies
+posFreq = (0:(kNyq-1))*(1/dataLen);
+% FFT of signal
+fftSig = fft(sigVec);
+% Discard negative frequencies
+fftSig = fftSig(1:kNyq);
+
+% Plot the periodogram
+figure;
+plot(posFreq, abs(fftSig));
+title('Step FM Sample')
+xlabel('Frequency')
+ylabel('Magnitude')
+
+% Generate the signal 
+s1 = gensfmsig(timeVec,A1,timea,freq0,freq1);
+s2 = gensfmsig(timeVec,A2,timea,freq0,freq1);
+s3 = gensfmsig(timeVec,A2,timea,freq0,freq1);
+
+SigVec = s1 +s2 + s3; 
+
+% Low pass filter 
+filtOrder = 30; 
+sampFreq = 1024;
+maxFreq = 2048; 
 
 
+timeVec = (0:(maxFreq-1))/sampFreq;
 
+% Design a low pass filter
+
+filtOrdr = 30; 
+b = fir1(filtordr, (2048/2) / (1024/2)); 
+
+%Apply filters
+filtSig = fftfilt(b,sigVec); 
 
