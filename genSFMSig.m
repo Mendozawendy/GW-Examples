96dd5f1ebf1c5edb00167ea678d9841eea23f896
soumya <soumya.d.mohanty@gmail.com>
2021-02-24 17:45:30 -0600

    SDM: Corrected the bugs (Again!)

--- genSFMSig.m
+++ genSFMSig.m
@@ -1,8 +1,8 @@
-function sigVec = genSFMSig(dataX,timea,freq0,freq1,snr,specgrmSFMSigDemo)
+function sigVec = genSFMSig(dataX,snr,timea,freq0,freq1)
  
 % TO DO
 % Generate a Step FM signal
-% S = GENSFMSIG(DATAX,TIMEA,FREQ0,FREQ1,SPECGRMSFMSIGDEMO)
+% S = GENSFMSIG(DATAX,TIMEA,FREQ0,FREQ1)
 % Generate a Step FM signal S. X is vector of the time stamps at which the value of the signal is to
 % be computed. 
 % SNR is the matched filtering signal-to-noise ratio of S.
@@ -10,7 +10,7 @@ function sigVec = genSFMSig(dataX,timea,freq0,freq1,snr,specgrmSFMSigDemo)
 % Timea is the time value for a peal of the curve. 
 % Freq0 is the frequency of sinusoidal oscillation.
 % Freq1 is the frequency of sinusoidal oscillation. 
-% specgrmSFMSigDemo is the function that generate assigned signal to make a spectrogram 
+
 %SNR * sin(2*pi*freq0*t) ; t <=; ta 
 %SNR * sin(2*pi*f1*(t-timea)+2*pi*freq0*timea) ; t >; ta 
 
@@ -18,14 +18,11 @@ function sigVec = genSFMSig(dataX,timea,freq0,freq1,snr,specgrmSFMSigDemo)
 %Wendy Mendoza, January 2021
 
 
-phaseVec = 2*pi*freq0*timea; 
-sfmSig = 2*pi*freq1*(dataX-timea)+2*pi*timea);
-sigVec = sin(2*pi*freq0*timea); t <=; ta;
-sigVec = sin(2*pi*freq1*(dataX-timea)+2*pi*timea); t >; ta;
+% phaseVec = 2*pi*freq0*timea; 
+% sfmSig = 2*pi*freq1*(dataX-timea)+2*pi*timea;
+sigVec = sin(2*pi*freq0*dataX).*( dataX <= timea);
+sigVec = sigVec+ sin(2*pi*freq1*(dataX-timea)+2*pi*freq0*timea).*(dataX >= timea);
 
 
 sigVec = snr*sigVec/norm(sigVec);
-
-end 
-
  
